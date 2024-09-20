e849487f3c521d1e64b9bf23a2ab638bb0bff05d
Mohamed Abo El Ala <89506214+MoHamedAboElAla@users.noreply.github.com>
2024-09-21 00:33:33 +0300

    Add files via upload

---
+++ Calculation.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculation
+    {
+        public  void Calculate(List<double> numbers)
+        {
+            // Ask the user to choose an operator.
+            Console.WriteLine("Choose an operator: +, -, *, or /");
+            string op = Console.ReadLine();
+
+            double result = numbers[0];
+
+            // Use a loop to do the math.
+            for (int i = 1; i < numbers.Count; i++)
+            {
+                switch (op)
+                {
+                    case "+":
+                        result += numbers[i];
+                        break;
+                    case "-":
+                        result -= numbers[i];
+                        break;
+                    case "*":
+                        result *= numbers[i];
+                        break;
+                    case "/":
+                        result /= numbers[i];
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operator");
+                        return;
+                }
+            }
+
+            // Print the result.
+            Console.WriteLine($"Your result: {result}");
+            Console.WriteLine("Press any key to close the Calculator console app...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ConsoleApp1
+    {
+
+    public class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Console Calculator in C#");
+                Console.WriteLine("------------------------\n");
+
+                List<double> numbers = new List<double>();
+                string input;
+                double number;
+
+               // Ask the user to Write numbers until they Write "done".
+                 Console.WriteLine("Write a number, and then press Enter. Write 'done' to finish.");
+                 while ((input = Console.ReadLine()) != "done")
+                {
+                    if (double.TryParse(input, out number))
+                    {
+                        numbers.Add(number);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid input. Please enter a valid number or 'done' to finish.");
+                    }
+                }
+
+            Calculation calculate = new Calculation();
+            calculate.Calculate(numbers);
+
+
+        }
+
+    
+        }
+    }
+
+
+
